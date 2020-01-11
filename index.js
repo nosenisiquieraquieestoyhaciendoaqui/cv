5d47b0efbe770b7dfc7e359c4f649d8e1d1c7401
nosenisiquieraquieestoyhaciendoaqui <59144808+nosenisiquieraquieestoyhaciendoaqui@users.noreply.github.com>
2020-01-10 23:47:59 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+
+
+for(var i = 0; i<document.querySelectorAll(".drum").length; i++){
+document.querySelectorAll(".drum")[i].addEventListener ("click", function (){
+
+  var boton = this.innerHTML;
+  sound(boton);
+});
+}
+
+document.addEventListener("keypress", function(event){
+sound(event.key);
+})
+
+function sound (key) {
+  switch (key) {
+    case "w":
+      new Audio("sounds/crash.mp3").play();
+      break;
+
+    case "a":
+      new Audio("sounds/kick-bass.mp3").play();
+      break;
+
+    case "s":
+      new Audio("sounds/snare.mp3").play();
+      break;
+
+    case "d":
+      new Audio("sounds/tom-1.mp3").play();
+      break;
+
+    case "j":
+      new Audio("sounds/tom-2.mp3").play();
+      break;
+
+    case "k":
+      new Audio("sounds/tom-3.mp3").play();
+      break;
+
+    case "l":
+      new Audio("sounds/tom-4.mp3").play();
+      break;
+
+    default: console.log("nop")
+
+  }
+}
